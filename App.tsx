67627e8fe28a6329c527b1c90a584ec69e64c1ef
aLe-kLaus <alessomklaus13@gmail.com>
2022-03-02 15:36:12 -0300

    add global theme

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 import AppLoading from 'expo-app-loading'
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
+import { ThemeProvider } from 'styled-components/native'
+import theme from 'src/theme'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,8 +15,9 @@ export default function App() {
   if (!fontsLoaded) return <AppLoading />
 
   return (
-    <View>
-    </View>
+   <ThemeProvider theme={theme}>
+
+   </ThemeProvider>
   );
 }
 
