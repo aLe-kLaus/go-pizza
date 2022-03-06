060a695d1e528ed177818fbe86307520783bddf9
aLe-kLaus <alessomklaus13@gmail.com>
2022-03-06 15:04:57 -0300

    finish sign in screen

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components/native";
+import { RectButton } from "react-native-gesture-handler";
+
+
+export type ButtonType = 'primary' | 'secondary'
+
+type ContainerProps = {
+  type?: ButtonType
+}
+
+export const Container = styled(RectButton)<ContainerProps>`
+  flex: 1;
+  max-height: 56px;
+  min-height: 56px;
+  border-radius: 12px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme, type}) => type === 'primary' ? theme.COLORS.SUCCESS_900 : theme.COLORS.PRIMARY_800};
+`
+
+export const Title = styled.Text`
+  font-size: 14px;
+  color: ${({theme}) => theme.COLORS.TITLE};
+  font-family: ${({theme}) => theme.FONTS.TEXT};
+`
+
+export const Load = styled.ActivityIndicator.attrs(({theme}) => ({
+  color: theme.COLORS.TITLE
+}))``;
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,26 @@
+import { KeyboardAvoidingView, Platform } from "react-native"
+import { Button } from "../../components/Button"
 import { Input } from "../../components/Input"
-import { Container } from "./styles"
+import { Brand, Container, Content, ForgotPassButton, ForgotPassLabel, Title } from "./styles"
+import brandImg from '../../assets/brand.png'
 
 export const SignIn = () => {
   return (
     <Container>
-      <Input type="secondary" placeholder="Email" autoCorrect={false} autoCapitalize="none"/>
-      <Input type="secondary" placeholder="Password" secureTextEntry/>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <Content>
+          <Brand source={brandImg}/>
+          <Title>Login</Title>
+          <Input type="secondary" placeholder="Email" autoCorrect={false} autoCapitalize="none"/>
+          <Input type="secondary" placeholder="Password" secureTextEntry/>
+          <ForgotPassButton>
+            <ForgotPassLabel>
+              Forgot my password
+            </ForgotPassLabel>
+          </ForgotPassButton>
+          <Button title="Sign In" type="secondary"/>
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
